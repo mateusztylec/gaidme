6b46a498e924d039408b3f253c3fbd3518f1dd26
Mateusz Tylec <mateusztylec@duck.com>
2024-10-06 20:06:35 +0200

    🐛 fix docs deployment 🐛

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,11 +1,18 @@
 import nextra from 'nextra'
- 
+
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx'
 })
- 
-export default withNextra()
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+}
+
+export default withNextra(nextConfig)
  
 // If you have other Next.js configurations, you can pass them as the parameter:
 // export default withNextra({ /* other next.js config */ })
